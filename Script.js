9221ab1d3fb9fae912f7bd01eeac03fe1313ec6a
Mikebrown4768 <52458720+Mikebrown4768@users.noreply.github.com>
2020-07-14 13:18:08 -0700

    Created a script.js file to support new features

---
+++ Script.js
@@ -0,0 +1,29 @@
+body {
+  background-color: lightblue;
+
+}
+
+h1 {
+  color: navy;
+  font-family: times sans-serif;
+}
+
+h2 {
+  font-family: sans-serif;
+  color: navy;
+}
+
+img {
+  float: left;
+  width: 100px;
+  margin: 10px;
+  border-color: mediumblue;
+  border-width: 6px;
+  border-style: solid;
+  border-radius: 10px;
+}
+
+p {
+  text-align: left;
+  font-family: inherit;
+  font-size: 25px;
